25547d3491a859579da616cbfafcbcd79fe12bf0
Carlos Alberto Dantas Filho <carlosdantas485@gmail.com>
2022-06-30 23:12:14 -0300

    create entitie deposit

---
+++ src/main/java/com/project/demo/entities/Deposit.java
@@ -0,0 +1,108 @@
+package com.project.demo.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_deposits")
+public class Deposit implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private Instant data;
+	private int day;
+	private int month;
+	private int year;
+
+	private Double balance;
+
+	public Deposit() {
+
+	}
+
+	public Deposit(Long id, Instant data, int day, int month, int year, Double balance) {
+		super();
+		this.id = id;
+		this.data = data;
+		this.day = day;
+		this.month = month;
+		this.year = year;
+		this.balance = balance;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getData() {
+		return data;
+	}
+
+	public void setData(Instant data) {
+		this.data = data;
+	}
+
+	public int getDay() {
+		return day;
+	}
+
+	public void setDay(int day) {
+		this.day = day;
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public void setMonth(int month) {
+		this.month = month;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Deposit other = (Deposit) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
