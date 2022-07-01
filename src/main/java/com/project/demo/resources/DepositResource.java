b3cb6d0b74182d43f3b4bc9db896e7034fa7ebbb
Carlos Alberto Dantas Filho <carlosdantas485@gmail.com>
2022-06-30 23:21:57 -0300

    create application-test

---
+++ src/main/java/com/project/demo/resources/DepositResource.java
@@ -0,0 +1,37 @@
+package com.project.demo.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.demo.entities.Deposit;
+import com.project.demo.services.DepositService;
+
+@RestController
+@RequestMapping(value = "/users")
+public class DepositResource {
+
+	@Autowired
+	private DepositService service;
+
+
+	@GetMapping
+	public ResponseEntity<List> findAll() {
+		List<Deposit> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Deposit> findById(@PathVariable Long id) {
+		Deposit obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+	
+
+}
---
+++ src/main/java/com/project/demo/services/DepositService.java
@@ -0,0 +1,27 @@
+package com.project.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.demo.entities.Deposit;
+import com.project.demo.repositories.DepositRepository;
+
+@Service
+public class DepositService {
+	
+	@Autowired
+	private DepositRepository repository;
+
+	public List<Deposit> findAll() {
+		return repository.findAll();
+	}
+
+	public Deposit findById(Long id) {
+		Optional<Deposit> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
